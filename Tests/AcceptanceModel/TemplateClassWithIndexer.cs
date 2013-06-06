42877a27ad2ea288a4d46d63fe88da4d4574ee41
Dave Kerr <dave_kerr@external.mckinsey.com>
2013-06-06 17:22:47 +0200

    Added missing pile

---
+++ Tests/AcceptanceModel/TemplateClassWithIndexer.cs
@@ -0,0 +1,36 @@
+﻿namespace AcceptanceModel
+{
+    public class TemplateClassWithIndexer<T>
+    {
+        private T[] arr = new T[100];
+
+        public T this[int i]
+        {
+            get
+            {
+                return arr[i];
+            }
+            set
+            {
+                arr[i] = value;
+            }
+        }
+    }
+
+    public class ClassWithIndexer
+    {
+        private int[] values = new int[100];
+
+        public int this[int i]
+        {
+            get
+            {
+                return values[i];
+            }
+            set
+            {
+                values[i] = value;
+            }
+        }
+    }
+}
